8f5c38d3bbd65d0f76f5908b09a8a4ea07211d9c
raelulu <raelulu23@gmail.com>
2022-11-23 19:22:17 +0900

    feat:11/23

--- 221122_express/index.js
+++ 221122_express/index.js
@@ -5,28 +5,32 @@ const port = 8080;
 app.set('view engine', 'ejs');
 
 /* app.use("/public", express.static("static")); */
-//express.static사용해서 미들웨어 등록...app.use안에.  ""안은 실제 존재하는 폴더이름.
-//클라이언트가 static폴더 접근하기위해 설정해줘야.
-//첫번째인자는 가상경로..원하는값넣어.app.use("/", express.static(""));이게  기본구조.""안에 넣어야..
-//static이라는 실제 존재하는 폴더에 public경로로 접근할수 있도록 함.
-// src="/public/img/cat.jpg"
+//static이라는 실제 존재하는 폴더에 public이라는 경로로 접근할수 있도록 함.
+// src="/public/img/cat.jpg"로 html에서 연결해서 사용하는것...
 
 app.use("/static", express.static("static"));
+//express안에 static이란 메소드가 있는데, 이 static을 사용해서 미들웨어를 등록하면된다.
+//미들웨어 등록하는 코드는 app.use 인데 이폴더를 찾아갈수 있게끔 이 인자 두개를 넘겨주면 됨. app.use안에다가 
+//이 뒤에 static안에 쓴("static"). 문자열은 실제로 존재하는 폴더이름.
+//static이란 폴더를 만들었고 client가 static이란 폴더에 접근할수있도록하기위해 코드를 작성하는것.
+//첫번째꺼는 가상경로. 원하는값 아무거나 써도 됨. 두번째로 들어올 인자는 무조건 express.static()으로 감싼다.
 
-// app.use(express.static("static"))???
+// app.use(express.static("static"))
 
 //localhost:8080
 app.get('/', (req, res)=>{
     res.send("hello express");
-})  //res 서버가 클라이언트측에 보내는 응답.그래서 send라는 메소드를 사용하는것../ req는 클라이언트가 서버측에 보내는 요청.
+});  //res 서버가 클라이언트측에 보내는 응답.그래서 send라는 메소드를 사용하는것../ req는 클라이언트가 서버측에 보내는 요청.
 //localhost:8080 들어가면 hello express나옴.
 
 
 //localhost:8080/test
 app.get('/test', (req, res)=>{
     res.sendFile( __dirname + "/views/index.html");
-})
-//__dirname: C:/~~~~~/~~~~/221122_express
+});
+//__dirname 이걸 사용하면 기본으로 지정되어있는 변수고 여기까지하면 알아서 index.js폴더가 여기 담겨있다 생각하면되고 "/views/index.html"적어주면됨.
+// __dirname: C:/~~~~~/~~~~/221122_express
+//sendFile 안에 절대경로 넣어야!!
 //파일 고친경우,,, 백엔드측 언어는 껐다 켜야 프론트측은 안그래드됨.
 
 app.use(express.urlencoded({extended: true})); // x-www-urlencoded 데이터해석.

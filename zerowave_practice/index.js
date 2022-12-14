5f320f42c22894ed82ac0bbe514b74b3a2b88dc5
raelulu <raelulu23@gmail.com>
2022-12-14 20:18:41 +0900

    feat:12/14

---
+++ zerowave_practice/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+app.set('view engine', 'ejs');
+
+app.use('/static', express.static(__dirname + '/static'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const router = require('./routes');
+app.use('/zerowave', router);
+
+//세션연결연습
+app.get("/login"), (req,res)=>{
+    res.render("login");
+}
+
+
+app.get('*', (req, res) => {
+    res.send('주소가 존재하지 않습니다. 다시 한 번 확인해주세요.');
+  });
+  
+
+app.listen(process.env.PORT, () => {
+    console.log('server open: ', process.env.PORT);
+  });
+  

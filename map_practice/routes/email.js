1074cbc21eaf058f3121c05e340e788b2efc4515
raelulu <raelulu23@gmail.com>
2022-12-27 16:42:44 +0900

    feat:12/27-2

--- map_practice/routes/email.js
+++ map_practice/routes/email.js
@@ -1,29 +1,7 @@
-const nodemailer = require("nodemailer");
-const email = {
-  host: "smtp.mailtrap.io",
-  port: 2525,
-  auth: {
-    user: "b7da9e9ed3e975",
-    pass: "b3ba22133c042b",
-  },
-};
+const express = require("express");
+const router = express.Router();
+const controller = require("../controller/Cemail");
 
-const send = async (option) => {
-  nodemailer.createTransport(email).sendMail(option, (error, info) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log(info);
-      return info.response;
-    }
-  });
-};
+router.post("/nodemailerTest", controller.email);
 
-let email_data = {
-  from: "raelulu23@gmail.com",
-  to: "raelulu23@gmail.com",
-  subject: "테스트 메일입니다. node js공부중 ",
-  text: "node js email test입니다.",
-};
-
-send(email_data);
+module.exports = router;
--- map_practice/routes/index.js
+++ map_practice/routes/index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var controller = require("../controller/Cvisitor");
 const router = express.Router();
 const nodemailer = require("nodemailer");
-const mailController = require("../controller/Cmail"); // 위의 메일 작성 코드의 파일 경로
+
 router.get("/", controller.main);
 
 router.get("/signup", controller.signup);

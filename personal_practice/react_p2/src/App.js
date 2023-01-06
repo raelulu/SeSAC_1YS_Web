30e8b4afc53c689a1d47c919d1c7759464788c7f
raelulu <raelulu23@gmail.com>
2023-01-06 23:51:27 +0900

    feat:01/06-2

--- personal_practice/react_p2/src/App.js
+++ personal_practice/react_p2/src/App.js
@@ -1,8 +1,22 @@
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return <div className="App"></div>;
+  let [teacher, setTeacher] = useState('이효석');
+
+  function customSetTeacher(name) {
+    setTeacher(name);
+    console.log(teacher);
+  }
+
+  return (
+    <div className="App">
+      <button onClick={() => customSetTeacher('tetz')}>영어로!</button>
+      <br />
+      <span>{teacher}</span>
+    </div>
+  );
 }
 
 export default App;

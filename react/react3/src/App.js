03772f44ca186b4de8afe2553379a290c11b198f
raelulu <raelulu23@gmail.com>
2023-01-07 17:48:37 +0900

    feat:01/07수업

---
+++ react/react3/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import CallCustom from './components/CallCustomObj';
+import CallCustomList from './components/CallCustomList';
+import CustomList from './components/CustomList';
+import CustomObj from './components/CustomObj';
+import ItemList from './components/ItemList';
+import List from './components/List';
+import ListChild from './components/ListChild';
+import ChangeObj from './components/ChangeObj';
+import CallChangeObj from './components/CallChangeObj';
+import Ex5 from './components/Ex5';
+
+function App() {
+  return (
+    <div className="App">
+      <Ex5 />
+    </div>
+  );
+}
+
+export default App;
---
+++ react/react3/src/components/ListChild.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+export default function ListChild(title, content) {
+
+  return (
+    <div>
+      <h2>{title}</h2>
+      <p>{content}</p>
+      <hr />
+    </div>
+  )
+}

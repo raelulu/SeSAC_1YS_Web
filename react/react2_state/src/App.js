125c1eb725a64c931c67c1e723c21377f54beddd
raelulu <raelulu23@gmail.com>
2023-01-07 09:20:03 +0900

    feat:1/07

--- react/react2_state/src/App.js
+++ react/react2_state/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 import Ex2 from './components/Ex2';
-import ObjectState from './components/ObjectState';
+import ObjectState from './components/ObjectState'
 
 function App() {
   return (

316cb941616d2777a047531a76029b7a4b65e58a
raelulu <raelulu23@gmail.com>
2023-01-10 08:29:35 +0900

    feat:01/10

--- react/react3_props/src/App.js
+++ react/react3_props/src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
+import CallCustomList from './components/CallCustomList';
 import ClassProps from './components/ClassProps';
+import CallCustomObj from './components/CustomObj';
+import Ex5 from './components/Ex5';
 import ItemList from './components/ItemList';
 import List from './components/List';
 import MainHeader from './components/MainHeader';
@@ -7,6 +10,9 @@ import MainHeader from './components/MainHeader';
 function App() {
   return (
     <div className="App">
+      <Ex5 />
+      <CallCustomObj />
+      <CallCustomList />
       <ItemList />
       <List />
       <MainHeader

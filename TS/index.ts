f2c8225c2b49a6751927a43c6da94c9511b28fad
raelulu <raelulu23@gmail.com>
2023-03-22 21:28:07 +0900

    feat: 03.22

---
+++ TS/index.ts
@@ -0,0 +1,21 @@
+let 이름 = "고래";
+let 나이 = 10;
+let 출생지역 = "제주";
+
+var 좋아하는거: { song: string; singer: string } = {
+  song: "PeacePiece",
+  singer: "BillEvans",
+};
+
+let project: {
+  member: string[];
+  days: number;
+  started: boolean;
+} = {
+  member: ["kim", "park"],
+  days: 30,
+  started: true,
+};
+
+let 회원들: (string | number)[] = [1, "2", 3];
+let 오브젝트: { data: string | number } = { data: 123 };

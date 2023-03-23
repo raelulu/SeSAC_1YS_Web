7dc216c16047c1eeac03d8054593ac293e1f2cf7
raelulu <raelulu23@gmail.com>
2023-03-23 22:37:03 +0900

    feat:03.23

--- TS/index.ts
+++ TS/index.ts
@@ -19,3 +19,20 @@ let project: {
 
 let 회원들: (string | number)[] = [1, "2", 3];
 let 오브젝트: { data: string | number } = { data: 123 };
+
+let user: string = "kim";
+let age: undefined | number = undefined;
+let married: boolean = false;
+let 철수: (string | number | undefined | boolean)[] = [user, age, married];
+
+let 학교: {
+  score: (number | boolean)[];
+  teacher: string;
+  friend: string | string[];
+} = {
+  score: [100, 97, 84],
+  teacher: "Phil",
+  friend: "John",
+};
+학교.score[4] = false;
+학교.friend = ["Lee", 학교.teacher];

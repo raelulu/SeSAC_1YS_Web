1aeb7475d649338182709950b1dd5ca46b225a9a
raelulu <raelulu23@gmail.com>
2023-04-10 17:30:46 +0900

    feat: 04.10

--- TS/index.ts
+++ TS/index.ts
@@ -277,3 +277,43 @@ interface Teacher extends Student {
 
 let 학생: Student = { name: "kim" };
 let 선생: Teacher = { name: "kim", age: 20 };
+
+interface Product {
+  brand: string;
+  serialNumber: number;
+  model: string[];
+}
+
+let 상품: Product = {
+  brand: "Samsung",
+  serialNumber: 1360,
+  model: ["TV", "phone"],
+};
+
+interface Cart {
+  product: string;
+  price: number;
+}
+
+let 장바구니: Cart[] = [
+  { product: "청소기", price: 7000 },
+  { product: "삼다수", price: 800 },
+];
+
+interface NewCart extends Cart {
+  card: boolean;
+}
+
+interface MathObj {
+  plus: (x: number, y: number) => number;
+  minus: (x: number, y: number) => number;
+}
+
+let 옵젝트: MathObj = {
+  plus(x, y) {
+    return x + y;
+  },
+  minus(x, y) {
+    return x - y;
+  },
+};

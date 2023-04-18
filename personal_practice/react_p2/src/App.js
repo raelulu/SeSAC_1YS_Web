6cfa0b403f5d806d102b2271afb0b47c04d36985
raelulu <raelulu23@gmail.com>
2023-04-18 12:19:17 +0900

    feat:04.18

--- personal_practice/react_p2/src/App.js
+++ personal_practice/react_p2/src/App.js
@@ -1,20 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useState, useTransition, useDeferredValue } from 'react';
 
 function App() {
-  let [teacher, setTeacher] = useState('이효석');
-
-  function customSetTeacher(name) {
-    setTeacher(name);
-    console.log(teacher);
-  }
+  // let [teacher, setTeacher] = useState('이효석');
+  let [name, setName] = useState('');
+  let [isPending, startTransition] = useTransition();
+  let state = useDeferredValue(name);
+  // function customSetTeacher(name) {
+  //   setTeacher(name);
+  //   console.log(teacher);
+  // }
+  let a = new Array(10000).fill(0);
 
   return (
     <div className="App">
-      <button onClick={() => customSetTeacher('tetz')}>영어로!</button>
+      {/* <button onClick={() => customSetTeacher('tetz')}>영어로!</button>
       <br />
-      <span>{teacher}</span>
+      <span>{teacher}</span> */}
+      <input
+        onChange={(e) => {
+          startTransition(() => {
+            setName(e.target.value);
+          });
+        }}
+      ></input>
+      {isPending
+        ? '로딩중'
+        : a.map(() => {
+            return <div>{state}</div>;
+          })}
     </div>
   );
 }

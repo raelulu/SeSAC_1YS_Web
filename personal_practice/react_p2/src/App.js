79794a9a998fefdffb1f4d111cbb0a4c20b3cf2b
raelulu <raelulu23@gmail.com>
2023-04-23 22:48:48 +0900

    feat:04.23

--- personal_practice/react_p2/src/App.js
+++ personal_practice/react_p2/src/App.js
@@ -4,21 +4,28 @@ import { useState, useTransition, useDeferredValue } from 'react';
 
 function App() {
   // let [teacher, setTeacher] = useState('이효석');
-  let [name, setName] = useState('');
-  let [isPending, startTransition] = useTransition();
-  let state = useDeferredValue(name);
+  // let [name, setName] = useState('');
+  // let [isPending, startTransition] = useTransition();
+  // let state = useDeferredValue(name);
   // function customSetTeacher(name) {
   //   setTeacher(name);
   //   console.log(teacher);
   // }
-  let a = new Array(10000).fill(0);
+  let [count, setCount] = useState(0);
+  let [age, setAge] = useState(20);
+  useEffect(() => {
+    if (count != 0 && count < 3) {
+      setAge(age + 1);
+    }
+  }, [count]);
+  // let a = new Array(10000).fill(0);
 
   return (
     <div className="App">
       {/* <button onClick={() => customSetTeacher('tetz')}>영어로!</button>
       <br />
       <span>{teacher}</span> */}
-      <input
+      {/* <input
         onChange={(e) => {
           startTransition(() => {
             setName(e.target.value);
@@ -29,7 +36,14 @@ function App() {
         ? '로딩중'
         : a.map(() => {
             return <div>{state}</div>;
-          })}
+          })} */}
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        누르면한살먹기
+      </button>
     </div>
   );
 }

234cd42ed39a50fe14773cb5fee36bc7690613e1
raelulu <raelulu23@gmail.com>
2023-04-24 22:40:11 +0900

    feat:04.24

---
+++ personal_practice/practice_js/Reduce/app.js
@@ -0,0 +1,13 @@
+const prices = [9.99, 1.5, 19.99, 49.99, 30.5];
+
+// let total = 0;
+// for (let price of prices) {
+//   total += price;
+// }
+// console.log(total);
+
+// const total = prices.reduce((total, price) => {
+//   return total + price;
+// });
+
+const total = prices.reduce((total, price) => total + price);
